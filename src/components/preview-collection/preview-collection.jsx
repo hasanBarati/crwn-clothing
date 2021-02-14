09da03663d70a62f29c249a7537355b5b3e5ec66
unknown <hasanbaratiasp@gmail.com>
2021-02-14 16:40:16 +0330

    created collection

--- src/components/preview-collection/preview-collection.jsx
+++ src/components/preview-collection/preview-collection.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import './preview-collection.style.scss'
+import CollectionItem from '../collection-item/collection-item.component'
 function CollectionPreview({title,items}) {
     return (
         <div className='collection-preview'>
-            <h1 className='title'></h1>
+            <h1 className='title'>{title.toUpperCase()}</h1>
             <div className='preview'>
                 {items
                 .filter((item,idx)=>idx<4)
-                .map(item=>(
-
-                    <div key={item.id}>{item.name}</div>
+                .map(({id,...otheritem})=>(
+                    <CollectionItem key={id}{...otheritem} />
                 ))}
             </div>
         </div>
