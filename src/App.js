80f72c514c09ab75b52e86e0a9e91795f77408fc
unknown <hasanbaratiasp@gmail.com>
2021-02-15 16:46:51 +0330

    adding header component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import {Route,Link} from 'react-router-dom'
+import {Route,Link,Switch} from 'react-router-dom'
 import './App.css';
 import HomePage from './pages/homepage/hompage.component'
 import ShopPage from './pages/shop/shop.component'
+import Header from './components/header/header-component'
 /*
 const TopicList=(props)=>{
   console.log(props)
@@ -41,9 +42,12 @@ const TopicList=(props)=>{
 function App() {
   return (
     <div className="App">
-      
-      <Route exact path='/' component={HomePage}/>
-      <Route exact path='/shop' component={ShopPage}/>
+      <Header />
+      <Switch>
+          <Route exact path='/' component={HomePage}/>
+          <Route exact path='/shop' component={ShopPage}/>
+      </Switch>
+    
     </div>
   );
 }
